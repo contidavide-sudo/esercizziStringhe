a01fecd6e82432786c5b07303ae745089ca7981b
contidavide-sudo <conti.davide@franchettisalviani.net>
2026-01-28 13:35:58 +0100

    Aggiungere i file di progetto.

---
+++ esercizio 2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace esercizio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esercizio 2");
+
+            string email = "conti.davide@francehttisalviani.net";
+
+            int l = email.Length;
+            int chiocciola = email.IndexOf("@");
+            string dopoChiocc = email.Substring(chiocciola + 1, l - chiocciola - 1);
+            Console.WriteLine($"La parte dopo la @ é: {dopoChiocc}");
+        }
+    }
+}
---
+++ esercizziStringhe/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace esercizziStringhe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esecizio 1");
+
+            string nome;
+
+            Console.Write("Inserisci il tuo nome completo: ");
+
+            nome = Console.ReadLine();
+
+            nome = nome.Trim();
+            nome = nome.ToUpper();
+            int l = nome.Length;
+
+            Console.WriteLine($"Il nome è {nome} ed è lungo {l}");
+
+        }
+    }
+}
