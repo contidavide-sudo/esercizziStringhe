fac568c890f07942bddccb0fcdf44f2579162ddb
contidavide-sudo <conti.davide@franchettisalviani.net>
2026-01-28 16:11:19 +0100

    proseguimento

---
+++ esercizio_3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace esercizio_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("esercizio 3");
+
+            string nome, cognome;
+
+            Console.Write("Inserisci il tuo nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Inserisci il tuo cognome: ");
+            cognome = Console.ReadLine();
+
+            char[] lettera1 = nome.ToCharArray();
+            char[] lettera2 = cognome.ToCharArray();
+
+            Console.WriteLine($"{lettera1[0]}.{lettera2[0]}.");
+        }
+    }
+}
