8aa29967a41768310b0946772afd3edda8553699
conti.davide <conti.davide@HP-009>
2026-01-29 09:53:00 +0100

    correziione errori

--- esercizio_3/Program.cs
+++ esercizio_3/Program.cs
@@ -15,10 +15,10 @@
             Console.Write("Inserisci il tuo cognome: ");
             cognome = Console.ReadLine();
 
-            char[] lettera1 = nome.ToCharArray();
-            char[] lettera2 = cognome.ToCharArray();
+            char lettera1 = nome[0];
+            char lettera2 = cognome[0];
 
-            Console.WriteLine($"{lettera1[0]}.{lettera2[0]}.");
+            Console.WriteLine($"{lettera1}.{lettera2}.");
         }
     }
 }
